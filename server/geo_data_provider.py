84d84c584fb5eb0cc68719ebb3257a97188e262e
Kacper Maksymowicz <kajmaksym@gmail.com>
2020-09-19 18:29:46 +0200

    Finish script

--- server/geo_data_provider.py
+++ server/geo_data_provider.py
@@ -3,15 +3,28 @@ import json
 class GeoDataProvider:
 
     def __init__(self):
+        with open('resources/british-cities-to-counties.json') as f:
+            cities_to_counties = json.load(f)
+        
+        county_list = list(cities_to_counties.values())
+
         with open('resources/british-isles-counties.geojson') as f:
             counties_geojsons = json.load(f)
 
         features = counties_geojsons['features']
         counties = {}
         for county in features:
+            name = ""
+            properties = county['properties']
+            for prop in properties.values():
+                if prop in county_list:
+                    name = prop
+
+            if not name:
+                continue
+
             data = {}
             data['type'] = county['type']
-            name = county['properties']['VARNAME_1']
             data['properties'] = {
                 'name': name
             }
