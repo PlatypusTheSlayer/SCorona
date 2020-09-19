6fda275da78814912f2fe026b27497d34abc9346
Kacper Maksymowicz <kajmaksym@gmail.com>
2020-09-19 14:27:51 +0200

    Use lowercase input for filtering

--- server/data_parser.py
+++ server/data_parser.py
@@ -14,25 +14,34 @@ class DataParser:
         with open('resources/british-cities-to-counties.json') as f:
             cities_to_counties = json.load(f)
 
-        self.cities = np.array(list(cities_to_counties.keys()))
         self.counties = np.unique(np.array(list(cities_to_counties.values())))
-        self.cities_and_counties = np.concatenate((self.cities, self.counties))
+        self.cities = np.array(list(cities_to_counties.keys()))
+        self.cities_and_counties = np.concatenate((self.counties, self.cities))
+        self.cities_and_counties_lower = np.char.lower(self.cities_and_counties)
         self.cities_to_counties = cities_to_counties
 
     def parse_text_for_location(self, article):
         words = np.array(nltk.word_tokenize(article))
+        words = np.char.lower(words)
+
         word_pairs = np.array([" ".join(bigram) for bigram in nltk.bigrams(words)])
         word_triplets = np.array([" ".join(trigram) for trigram in nltk.ngrams(words, 3)])
 
-        three_word_locations = word_triplets[np.isin(word_triplets, self.cities_and_counties)]
-        two_word_locations = word_pairs[np.isin(word_pairs, self.cities_and_counties)]
-        single_word_locations = words[np.isin(words, self.cities_and_counties)]
+        three_word_locations = word_triplets[np.isin(word_triplets, self.cities_and_counties_lower)]
+        two_word_locations = word_pairs[np.isin(word_pairs, self.cities_and_counties_lower)]
+        single_word_locations = words[np.isin(words, self.cities_and_counties_lower)]
 
         filtered_locations = np.concatenate((three_word_locations, two_word_locations, single_word_locations))
-        locations, counts = np.unique(filtered_locations, return_counts=True)
-
+        possible_locations, counts = np.unique(filtered_locations, return_counts=True)
+        
         try:
-            return locations[np.argmax(counts)]
+            # Find indices with most occurences
+            indices = np.argwhere(counts == np.max(counts))
+            # Out of these filter the longest matching name
+            location = max(possible_locations[indices].tolist())[0]
+            # Find its index and return the correctly cased version
+            location_index = np.argwhere(self.cities_and_counties_lower == location)
+            return self.cities_and_counties[location_index].flatten()[0]
         except:
             return "NO_LOCATION"
 
@@ -53,7 +62,7 @@ if __name__ == "__main__":
     article6 = """Lorem ipsum dolor sit Greater Manchester, Greater Manchester adipiscing elit. Maecenas eu aliquet Greater Manchester, nec Surrey metus. Greater Manchester ornare felis in urna Surrey, eu Greater London quam malesuada. Aenean facilisis nibh ut enim blandit, ac hendrerit libero placerat. Nunc sit amet dolor arcu. Sed convallis molestie placerat. Cras ut mauris elementum, accumsan mauris accumsan, consectetur turpis."""
     article7 = """Lorem ipsum dolor sit East Sussex, East Sussex adipiscing elit. Maecenas eu aliquet East Sussex, nec Surrey metus. East Sussex ornare felis in urna Surrey, eu Greater London quam malesuada. Aenean facilisis nibh ut enim blandit, ac hendrerit libero placerat. Nunc sit amet dolor arcu. Sed convallis molestie placerat. East Sussex ut mauris elementum, accumsan mauris accumsan, consectetur turpis."""
     article8 = """Lorem ipsum dolor sit Tyne and Wear, Tyne and Wear adipiscing elit. Maecenas eu aliquet Tyne and Wear, nec Surrey metus. Tyne and Wear ornare felis in urna Surrey, eu Greater London quam malesuada. Aenean facilisis nibh ut enim blandit, ac hendrerit libero placerat. Tyne and Wear sit amet dolor arcu. Sed convallis molestie placerat. Tyne and Wear ut mauris elementum, accumsan mauris accumsan, consectetur turpis."""
-    article9 = """Lorem ipsum dolor sit Newcastle, Newcastle adipiscing elit. Maecenas eu aliquet Newcastle, nec Surrey metus. Newcastle ornare felis in urna Surrey, eu Newcastle quam malesuada. Aenean facilisis nibh ut enim blandit, ac hendrerit libero placerat. Tyne and Wear sit amet dolor arcu. Sed Newcastle molestie placerat. Tyne and Wear ut mauris Newcastle, accumsan mauris accumsan, consectetur turpis."""
+    article9 = """asdas asdasd sadfkjh adsfjh dsaf Newcastle upon Tyne shjd Newcastle upon Tyne sadkjh jskadh Newcastle upon Tyne Newcastle upon Tyne aksdf dsf, asfd Newcastle upon Tyne"""
     with open('resources/example-article.txt', 'r') as f:
         article10 = f.read()
 
@@ -68,5 +77,5 @@ if __name__ == "__main__":
     print("Article unique two word county, location: %s, county: %s." % (data_parser.parse_text_for_location(article6), data_parser.parse_text_for_county(article6)))
     print("Article non-unique two word county, location: %s, county: %s." % (data_parser.parse_text_for_location(article7), data_parser.parse_text_for_county(article7)))
     print("Article three word county, location: %s, county: %s." % (data_parser.parse_text_for_location(article8), data_parser.parse_text_for_county(article8)))
-    print("Article Newcastle check, location: %s, county: %s." % (data_parser.parse_text_for_location(article9), data_parser.parse_text_for_county(article9)))
+    print("Article three word city, location: %s, county: %s." % (data_parser.parse_text_for_location(article9), data_parser.parse_text_for_county(article9)))
     print("Article long article check, location: %s, county: %s." % (data_parser.parse_text_for_location(article10), data_parser.parse_text_for_county(article10)))
